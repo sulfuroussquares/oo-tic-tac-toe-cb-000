bda6843c9655d2d1eb18e1fa69b2e4a107876aaf
BRIAN BETANCOURT <bbetancova@gmail.com>
2018-06-26 04:02:31 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,22 +1,20 @@
 class TicTacToe
-  def initialize
-    @board= Array.new(9, " ")
-  end
+  def initialize(board = nil)
+      @board = board || Array.new(9, " ")
+    end
 
-  def current_player(board)
-  turn_count(board) % 2 == 0 ? "X" : "O"
-end
+    def current_player
+      turn_count % 2 == 0 ? "X" : "O"
+    end
 
-def turn_count(board)
-  board.count{|token| token == "X" || token == "O"}
-end
+    def turn_count
+      @board.count{|token| token == "X" || token == "O"}
+    end
 
-def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
-
+    def display_board
+      puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+      puts "-----------"
+      puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+      puts "-----------"
+      puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
 end
