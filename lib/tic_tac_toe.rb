0fc860100caedc024e3531b906d4fd1dd941fa25
BRIAN BETANCOURT <bbetancova@gmail.com>
2018-06-26 04:05:29 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -36,5 +36,25 @@ def input_to_index(input)
   return index
 end
 
+def valid_move?(board, index)
+  if (index.between?(0,8))
+    if (position_taken?(board, index) == false)
+      return true
+    end
+  end
+end
+
+
+def position_taken?(board, index)
+  if (board[index] == "" || board[index] == " " || board[index] == nil)
+    return false
+  else
+    return true
+end
+end
+
+def move(board, input, letter)
+  board[input] = letter
+end
 
 end
